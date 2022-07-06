d55e0d083823348b118dc7d1d3a1d78684e585b4
Clemence Dumont <cdumont90@gmail.com>
2022-07-06 17:41:06 +0200

    cards destinations

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
+
 9.times do
   destination = Destination.new(
     city: Faker::Address.city,
